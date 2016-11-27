f81e6b60559f2ba21e222e7090d46108c59ceecf
Andrew Harcourt <andrewh@uglybugger.org>
2016-11-27 21:47:48 +1000

    Added placeholder test projects

---
+++ src/DDDBrisbane2016.Tests.Unit/WhenTheTestHarnessRuns.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace DDDBrisbane2016.Tests.Unit
+{
+    public class WhenTheTestHarnessRuns
+    {
+        [Test]
+        public void ThePeopleShallRejoice()
+        {
+        }
+    }
+}
