ad8ab5b17c06e43d510795fc0cd79f54c288affd
Andrew Harcourt <andrewh@uglybugger.org>
2016-11-28 17:37:47 +1000

    Add placeholder test

---
+++ src/DDDBrisbane2016.Tests.Integration/WhenTheTestHarnessRuns.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace DDDBrisbane2016.Tests.Integration
+{
+    public class WhenTheTestHarnessRuns
+    {
+        [Test]
+        public void ThePeopleShallRejoice()
+        {
+        }
+    }
+}
