794367bb268745c75dc409ff82eb313afae0928d
Andrew Harcourt <andrewh@uglybugger.org>
2016-11-30 21:09:31 +1000

    Add healthcheck integration test

---
+++ src/DDDBrisbane2016.Tests.Integration/AppSettings/WebSiteBaseUrl.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using ConfigInjector;
+
+namespace DDDBrisbane2016.Tests.Integration.AppSettings
+{
+    public class WebSiteBaseUrl : ConfigurationSetting<Uri>
+    {
+    }
+}
---
+++ src/DDDBrisbane2016.Tests.Integration/WhenHittingTheHealthCheckEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using ConfigInjector.QuickAndDirty;
+using DDDBrisbane2016.Tests.Integration.AppSettings;
+using NUnit.Framework;
+
+namespace DDDBrisbane2016.Tests.Integration
+{
+    public class WhenHittingTheHealthCheckEndpoint
+    {
+        [Test]
+        public async Task WeShouldGetASuccessfulResponse()
+        {
+            var healthCheckEndpoint = DefaultSettingsReader.Get<WebSiteBaseUrl>();
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(healthCheckEndpoint);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
