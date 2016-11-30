643abceb27e82a55ff2bbaba8203ff47540de0b5
Andrew Harcourt <andrewh@uglybugger.org>
2016-11-30 21:04:57 +1000

    Very first cut of slideshow controller. Primitive.

---
+++ src/DDDBrisbane2016.Web/Controllers/SlideshowController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace DDDBrisbane2016.Web.Controllers
+{
+    public class SlideshowController : Controller
+    {
+        [HttpGet]
+        [OutputCache(Duration = 0)]
+        public ActionResult Start()
+        {
+            return Redirect("/slideshow/display/howtoplay");
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 0)]
+        public ActionResult Display(string id)
+        {
+            var viewName = id;
+            return View(viewName);
+        }
+    }
+}
