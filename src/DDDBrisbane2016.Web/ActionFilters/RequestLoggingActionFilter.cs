01da3042342f4abe7f6826e8025e098cc2f4e96c
Andrew Harcourt <andrew.harcourt@thoughtworks.com>
2016-12-01 21:42:57 +1100

    First cut of logging action filter. Does not work yet.

---
+++ src/DDDBrisbane2016.Web/ActionFilters/RequestLoggingActionFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace DDDBrisbane2016.Web.ActionFilters
+{
+    public class RequestLoggingActionFilter: IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+    }
+}
--- src/DDDBrisbane2016.Web/AutofacModules/MvcModule.cs
+++ src/DDDBrisbane2016.Web/AutofacModules/MvcModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
@@ -11,12 +12,21 @@ namespace DDDBrisbane2016.Web.AutofacModules
         {
             base.Load(builder);
 
+            builder.RegisterModule<AutofacWebTypesModule>();
+
             builder.RegisterControllers(ThisAssembly);
             builder.RegisterFilterProvider();
 
+            foreach (var type in ThisAssembly.DefinedTypes.Where(t => t.IsAssignableTo<IActionFilter>()))
+            {
+                builder.RegisterType(type)
+                       .AsActionFilterFor<IController>()
+                       .InstancePerLifetimeScope();
+            }
+
             builder.Register(c => new UrlHelper(HttpContext.Current.Request.RequestContext))
-                .As<UrlHelper>()
-                .InstancePerLifetimeScope();
+                   .As<UrlHelper>()
+                   .InstancePerLifetimeScope();
         }
     }
 }
