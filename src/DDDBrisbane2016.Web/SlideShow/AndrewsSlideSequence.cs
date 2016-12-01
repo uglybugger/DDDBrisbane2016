01b23fd3f098c07b3f47a0a32b3ec21b9bb3fea4
Andrew Harcourt <andrewh@uglybugger.org>
2016-12-01 19:58:26 +1000

    Beginnings of actual content

--- src/DDDBrisbane2016.Web/SlideShow/AndrewsSlideSequence.cs
+++ src/DDDBrisbane2016.Web/SlideShow/AndrewsSlideSequence.cs
@@ -3,6 +3,7 @@ namespace DDDBrisbane2016.Web.SlideShow
     public class AndrewsSlideSequence : SlideNameSequence
     {
         public AndrewsSlideSequence() : base(
+            "AboutMe",
             "HowToPlay",
             "ThankYou"
             )
