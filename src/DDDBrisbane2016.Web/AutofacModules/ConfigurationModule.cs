17bb8b4240a0a02c7602167d730b5463cd1b1cac
Andrew Harcourt <andrewh@uglybugger.org>
2016-12-03 07:32:00 +1000

    Wire in ConfigInjector properly

---
+++ src/DDDBrisbane2016.Web/AutofacModules/ConfigurationModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using ConfigInjector.Configuration;
+
+namespace DDDBrisbane2016.Web.AutofacModules
+{
+    public class ConfigurationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            ConfigurationConfigurator.RegisterConfigurationSettings()
+                .FromAssemblies(ThisAssembly)
+                .RegisterWithContainer(configSetting => builder.RegisterInstance(configSetting)
+                    .AsSelf()
+                    .SingleInstance())
+                .AllowConfigurationEntriesThatDoNotHaveSettingsClasses(true)
+                .DoYourThing();
+        }
+    }
+}
